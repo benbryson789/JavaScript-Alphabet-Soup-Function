3c45472881be77cfe4c9f3b8df575b1feae555f2
Debra Shelby <deborashelby@gmail.com>
2021-03-31 22:45:16 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function AlphabetSoup(input)
+  {
+return input.split('').sort().join('');
+  }
+
+console.log(AlphabetSoup("coderbyte"))//bcdeeorty
+
+console.log(AlphabetSoup("hooplah"))//ahhloop
+   
